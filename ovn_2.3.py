433a963fff17b82423d5871472c19d19246db8bd
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-09-28 13:53:11 +0200

    brum

--- ovn_2.3.py
+++ ovn_2.3.py
@@ -1,5 +1,4 @@
-import math#importerar math
 svar=input("vilken grader i farenheit: ")#tar in vilken farenheit det är
-f=int(svar)#omvanlar svaret till formel formen
+f=float(svar)#omvandlar svaret till formel formen
 c=(f-32)*5/9#räknar om farenheit till celsius
 print(c,"grader celsius")#skriver ut vilken grader celsius det är
