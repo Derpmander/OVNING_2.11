7a62db69517baaf2a3ba5b5268aeccdb5c333777
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-09-28 14:25:32 +0200

    Update ovn_2.3.py

--- ovn_2.3.py
+++ ovn_2.3.py
@@ -1,4 +1,4 @@
-svar=input("vilken grader i farenheit: ")#tar in vilken farenheit det är
+svar=input("Vilken grader i farenheit?: ")#tar in vilken farenheit det är ## kan göras bättre
 f=float(svar)#omvandlar svaret till formel formen
 c=(f-32)*5/9#räknar om farenheit till celsius
 print(c,"grader celsius")#skriver ut vilken grader celsius det är
