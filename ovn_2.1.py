fdc8af89e6e1b3765d11f490ecf1db7935b9fe11
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-09-28 13:44:35 +0200

    kommentarer

--- ovn_2.1.py
+++ ovn_2.1.py
@@ -1,8 +1,8 @@
-import math
-i=float(input("Mätarställning i dag?: "))
-v=float(input("Mätarställning för ett år sedan?: "))
-m=i-v
-print("Antal mil körda?: ",m)
-b=float(input("Antal liter?: "))
-p=b/m
-print("Förbrukning per mil:",p)
+import math#importerar math
+i=float(input("Mätarställning i dag?: "))#Läser in dagens mätarställning
+v=float(input("Mätarställning för ett år sedan?: "))#läser in mätarställningne för ett år sedan
+m=i-v#Räknar ut hur många mil man åkt på ett år
+print("Antal mil körda?: ",m)#Läser in atal mil körda
+b=float(input("Antal liter?: "))#läser in antal liter
+p=b/m#Räknar ut bensin förbrukningen per mil
+print("Förbrukning per mil:",p)#Skriver ut förbrukningen per mil
