bc8ab4b405d4ab2806527a546865b04179af8722
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-09-28 12:52:38 +0200

    kommnetarer

--- ovn_2.11_2.2.py
+++ ovn_2.11_2.2.py
@@ -1,5 +1,5 @@
-import math
-r=float(input("Vad är sfärens radie?:"))
-v=4*math.pi*r**3/3
-a=4*math.pi*r**2
-print("Sfärens area är:",a,"Sfärens volym är:",v)
+import math #Importerar modulen math för att sedan kunna använda den
+r=float(input("Vad är sfärens radie?:"))#Frågar efter radien
+v=4*math.pi*r**3/3#räknar ut volymen från radiens data
+a=4*math.pi*r**2#räknar ut arean från radiens data
+print("Sfärens area är:",a,"Sfärens volym är:",v)#skriver ut både arean och volymen
