641f5301848b8117cd172848431b3356d49f1623
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-09-28 13:06:27 +0200

    uppgift 2.3 med kommentarer

---
+++ ovn_2.3.py
@@ -0,0 +1,5 @@
+import math#importerar math
+svar=input("vilken grader i farenheit: ")#tar in vilken farenheit det är
+f=int(svar)#omvanlar svaret till formel formen
+c=(f-32)*5/9#räknar om farenheit till celsius
+print(c,"grader celsius")
